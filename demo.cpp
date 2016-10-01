8326a0f22db42eb787a5a64ee7c94b48f3817cd5
lincaiming <lincaiming5837@qq.com>
2016-10-01 19:14:50 +0800

    v6_1_11 means filter

--- demo.cpp
+++ demo.cpp
@@ -1,16 +1,24 @@
-#include <opencv2/opencv.hpp>
+#include <iostream>  
+#include <opencv2/core/core.hpp>  
+#include <opencv2/highgui/highgui.hpp>  
+#include <opencv2/imgproc/imgproc.hpp>  
+using namespace cv;  
+using namespace std;
 
-//using namespace cv;
-using namespace cv;
 
 int main(int argc, char**argv)
 {
 
 	Mat image;
-//	image = imread("1.jpg");
 	image = imread("1.jpg");
+	
+
 	imshow("Display image", image);
 	
+	Mat out;
+	boxFilter(image, out, -1, Size(5, 5));
+
+	imshow("means filter [result]", out);
 	waitKey();
 	return 0;
 }
